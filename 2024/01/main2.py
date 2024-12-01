8f91546f26318a61faffe4713959abb81bb182d9
tillepille <github@tillepille.io>
2024-12-01 23:37:55 +0100

    day 1 part 2

---
+++ 2024/01/main2.py
@@ -0,0 +1,19 @@
+import sys, re
+
+file = open(sys.argv[1]).read().strip()
+
+result = 0
+
+left = []
+right =  []
+for i,line in enumerate(file.split('\n')):
+  left.append(int(line.split('   ')[0]))
+  right.append(int(line.split('   ')[1]))
+
+result = 0
+for i, item in enumerate(left):
+  similarity = right.count(item) * item
+  print(similarity)
+  result += similarity
+
+print(result)
